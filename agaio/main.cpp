2d447120f965fa2f5ee332e2d599ae2c943bbcec
SEGARS <Vim_Branet@mail.ru>
2025-06-25 10:01:13 +0500

    работа дома

--- agaio/main.cpp
+++ agaio/main.cpp
@@ -1,7 +1,8 @@
 ﻿#include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include <random>
-
+#include <iostream>
+#include <chrono>
 #include <vector>
 
 using namespace std;
@@ -11,7 +12,12 @@ using namespace sf;
 Размер мира будет 1000х1000
 */
 
-int puls = 500;
+int puls = 20;
+
+/*
+Надо будет избавится от бага чёрных дыр =) или сделать фон белыйм, но и в таком случаи будут тогда
+белын шалрики =)
+*/
 
 //--------------------------------------------------------------------------------
 CircleShape getShsr(float x, float y)//функция генерирует кружок, и устанавливает все необходимые параметры
@@ -64,12 +70,26 @@ int main()
     glShsr.setFillColor(Color::Red);    // Цвет заливки фигуры
     glShsr.setPosition({ 400 - 20, 300 - 20 });
 
-    Direction direction = Direction::stop;
+    Clock frameClock; //счетчик, чтобы считать время, за которое выводится кадр(frame), 1 сек / на это время = fps (кадры в сек)
 
+    float speed = 400.f; // скорость движения пикселей в секунду
+    Vector2f directionVector(0.0f, 0.0f); // вектор направления, сделали взамен enum direction
+
+    window.setFramerateLimit(60); //ограничим fps чтобы не было рывков в движении шариков 
+    window.setVerticalSyncEnabled(true); //оптимизация тоже для увеличения плавности
+    // над плавностью еще поработаем дальше
 
     // Start the game loop
     while (window.isOpen())
     {
+        // переменная в которой считаем время за которое выводится кадр
+        // понятие deltaTime есть во многих движках, и везде оно обозначает одно и то же - время за которое выводится кадр
+        float deltaTime = frameClock.getElapsedTime().asSeconds();
+
+        //std::cout << deltaTime << std::endl;
+        cout << "fps: " << 1.f / deltaTime << "\n"; // просто для наглядности сколько сейчас у нас fps
+        frameClock.restart(); // важно сбрасывать счетчик каждый кадр, чтобы считать именно время кадра
+
         // Process events
         while (const std::optional event = window.pollEvent())
         {
@@ -81,43 +101,63 @@ int main()
             {
                 if (Keyboard::isKeyPressed(sf::Keyboard::Key::Down) || Keyboard::isKeyPressed(sf::Keyboard::Key::S))
                 {
-                    direction = Direction::down;
+                    directionVector.y = -1.0f;
                 }
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right) || Keyboard::isKeyPressed(sf::Keyboard::Key::D))
                 {
-                    direction = Direction::right;
+                    directionVector.x = -1.0f;
                 }
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left) || Keyboard::isKeyPressed(sf::Keyboard::Key::A))
                 {
-                    direction = Direction::left;
+                    directionVector.x = 1.0f;
                 }
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) || Keyboard::isKeyPressed(sf::Keyboard::Key::W))
                 {
-                    direction = Direction::up;
+                    directionVector.y = 1.0f;
                 }
             }
         }
 
+        // тут у нас появляется понятие нормализации вектора
+        // нормализация это приведение вектора к длине равной 1, но сохраняя направление вектора
+        // делаем это чтобы наши шарики двигались с одинковой скоростью по диагонали
+        // длина вектора считается как квадратный корень от суммы квадратов x и y
+        // sqrt(x^2 + y^2) где sqrt - квадратный корень, ^2 - возведение в степень 2 или в квадрат
+        // на пример x = 1, y = 0 => sqrt(1^2 + 0^2) = sqrt(1 + 0) = sqrt(1) = 1
+        // на пример x = 1, y = 1 => sqrt(1^2 + 1^2) = sqrt(1 + 1) = sqrt(2) = 1.41 , т.е. почти на 40% быстрее двигаться по диагонали
+        // после нормализации получаем x = 0.71 y = 0.71, и длина вектора примерно 1
+        if (directionVector != Vector2f(0.0f, 0.0f))
+        {
+            //считаем на сколько нужно сдвинуть шарики, учитывая время кадра
+            //если скорость 400 пикселей в сек, а кадр выводится за 0,0004 секунды, 
+            //значит нам нужно переместить в текущем кадре шарики всего на 1,6 пикселя, и тогда за 1 секунду мы переместимся на 400 пикселей
+            float frameSpeed = speed * deltaTime;
+            // { 1, 0 } -> { 1, 0 }
+            // { 1, 1 } -> { 0.71, 0.71 }
+            //// { 0.24, 0 }
+            // нормализируем вектор направления и только после этого умножаем на скорость в текущем кадре
+            directionVector = directionVector.normalized() * frameSpeed;
+            for (int i = 0; i < shsr.size(); i++)
+            {
+                shsr[i].move(directionVector);
+            }
+            directionVector = Vector2f(0.0f, 0.0f);// не забываем сбросить вектор направления, иначе бует двигаться даже после отпускания кнопки движения
+        }
+
+        //цикл определения столкновения с шариками
         for (int i = 0; i < shsr.size(); i++)
         {
-            if (direction == Direction::down)
+            //определяем расстояние между нашим шариком и шариком из вектора
+            //такой вектор можем получить просто отняв позицию шарика из вектора от позиции нашего нарика
+            Vector2f rasoyanie = glShsr.getPosition() - shsr[i].getPosition();
+            //считаем сумму радиусов нашего шарика и шарика из вектора
+            int sumRad = glShsr.getRadius() + shsr[i].getRadius();
+            //если расстояние между шариками меньше или равно сумме радиусов - значит шарики столкнулись
+            if (rasoyanie.length() <= sumRad) 
             {
-                shsr[i].move(Vector2f(0.0, -3.0));
-            }
-            if(direction == Direction::right)
-            {
-                shsr[i].move(Vector2f(-3.0, 0.0));
-            }
-            if (direction == Direction::left)
-            {
-                shsr[i].move(Vector2f(+3.0, 0.0));
-            }
-            if (direction == Direction::up)
-            {
-                shsr[i].move(Vector2f(0.0, +3.0));
+                cout << "Collision detected!" << endl;
             }
         }
-        direction = Direction::stop;
 
         // Clear screen
         window.clear();
